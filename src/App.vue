8aaadc7bbd4a1640060d3214e86c3b768b4a4c5f
jesse <jsoldat@hotmail.com>
2017-01-27 10:19:39 +0700

    learning the basics

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <test></test>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+
+import Test from './components/test'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Test
   }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="test">
+		<span v-html="title">{{title}}</span>
+		<div v-if="showName">
+			<p>{{user.fName}}</p>
+			<p>{{user.lName}}</p>
+		</div>
+		<div v-else="showName">
+			<p>No User Logged Available</p>
+		</div>
+		 
+		<p>{{user.age}}</p>
+		<p v-text="user.hobby"></p>
+
+		<ul>
+			<li v-for="item in items">
+				{{item.title}}
+			</li>
+		</ul>
+	</div>
+	
+</template>
+
+<script>
+	export default {
+		name: 'test',
+		data () {
+			return {
+				title: "<h2>Hello World</h2>",
+				user: {
+					fName: 'Jesse',
+					lName: 'Soldat',
+					age: 36,
+					hobby: 'Programming'
+				},
+				showName: false,
+				items: [
+					{title: 'Learn React'},
+					{title: 'Learn Angular 2'},
+					{title: 'Learn Graph QL'},
+					{title: 'Learn Node'},
+					{title: 'Learn Vue'}
+				]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
